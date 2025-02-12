228f94ef7ce84cdd98d2299149f89b88b3d0c5e9
HAL <68320771+HALQME@users.noreply.github.com>
2025-02-13 07:54:29 +0900

    sitemap統合のための設定を追加し、robot.txtを新規作成

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,24 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-    output: "static",
-    site: "https://omu-aikido.com",
-    redirects: {
-        "/signin": "https://app.omu-aikido.com/sign-in",
-    },
-    vite: {
-        build: {
-            rollupOptions: {
-                external: ["omu-aikido-app"],
-            },
-        },
-    },
+  output: "static",
+  site: "https://omu-aikido.com",
+
+  redirects: {
+      "/signin": "https://app.omu-aikido.com/sign-in",
+  },
+
+  vite: {
+      build: {
+          rollupOptions: {
+              external: ["omu-aikido-app"],
+          },
+      },
+  },
+
+  integrations: [sitemap()],
 });
