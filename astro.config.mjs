95f6e9fbc238ee5826114b7705aa5bb331968ae1
HAL <68320771+HALQME@users.noreply.github.com>
2026-02-04 08:56:16 +0900

    Enable UnoCSS reset injection

--- astro.config.mjs
+++ astro.config.mjs
@@ -40,6 +40,6 @@ export default defineConfig({
       description:
         "大阪公立大学で活動する体育会所属の合気道団体である、大阪公立大学合氣道部のホームページです。\nなお、当団体は「大阪公立大学合氣道部」であり、「大阪公立大学体育会合気道部」とは別の団体です。",
     }),
-    UnoCSS(),
+    UnoCSS({injectReset: true}),
   ],
 });
